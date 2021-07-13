1ceea7468607f92b3540a6cd6e43c994541eded4
Danielle <danielleyuri-jp@hotmail.com>
2021-07-13 15:21:00 -0300

    *created instance attributes name and ange

--- Orientation and Object/people.py
+++ Orientation and Object/people.py
@@ -1,10 +1,19 @@
 class People:
-    def greet (self):
+    def __init__(self, name=None, age=15):
+        self.age = age
+        self.name = name
+
+
+    def greet(self):
         return f'Hello {id(self)}'
 
 
 if __name__ == '__main__':
-    p = People()
+    p = People('Luna')
     print(People.greet(p))
     print(id(p))
     print(p.greet())
+    print(p.name)
+    p.name = 'Shiny'
+    print(p.name)
+    print(p.age)
