d8bf3e2513e7276fb26b7ad81a419e836f88699b
Danielle <danielleyuri-jp@hotmail.com>
2021-07-13 15:14:23 -0300

    *Created method Greet.

--- Orientation and Object/people.py
+++ Orientation and Object/people.py
@@ -1,2 +1,10 @@
 class People:
-    pass
+    def greet (self):
+        return f'Hello {id(self)}'
+
+
+if __name__ == '__main__':
+    p = People()
+    print(People.greet(p))
+    print(id(p))
+    print(p.greet())
