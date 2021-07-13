0d1e4573fb4ec4dfcd66a349e2fbb342a6a543bd
Danielle <danielleyuri-jp@hotmail.com>
2021-07-13 15:32:56 -0300

    *created and removed dynamic attribute of people type objects

--- Orientation and Object/people.py
+++ Orientation and Object/people.py
@@ -4,18 +4,30 @@ class People:
         self.name = name
         self.child = list(child)
 
-
     def greet(self):
         return f'Hello {id(self)}'
 
+    def finish(self):
+        return f'See you later {id(self)}'
+
+    def finishs(self):
+        return f'Good Night {id(self)}'
+
 
 if __name__ == '__main__':
-    luna = People(name='Luna')
     shiny = People(name='Shiny')
+    luna = People(shiny, name='Luna')
     print(People.greet(luna))
     print(id(luna))
     print(luna.greet())
     print(luna.name)
     print(luna.age)
-    for childs in shiny.child:
+    for childs in luna.child:
         print(childs.name)
+    luna.lastname = 'Santos'
+    del luna.child
+    print(luna.__dict__)
+    print(shiny.__dict__)
+
+    print(People.finish(luna))
+    print(People.finishs(luna))
